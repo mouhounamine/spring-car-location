cc844dc0cb558417bfb73024d787c297b04c4d80
amineproject <aminedatabases@gmail.com>
2025-01-13 22:53:17 +0100

    dockerized mysql + init db while launching the container

--- db_utils/script.sql
+++ db_utils/script.sql
@@ -1,16 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
@@ -24,8 +11,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`tour` (
                                              `tour_date` DATE NOT NULL,
                                              `tour_time` TIME NOT NULL,
                                              PRIMARY KEY (`tour_id`),
-    INDEX `FK_property_id` (`property_id` ASC) INVISIBLE,
-    INDEX `FK_client_id` (`client_id` ASC) VISIBLE)
+    INDEX `FK_property_id` (`property_id` ASC) ,
+    INDEX `FK_client_id` (`client_id` ASC) )
     ENGINE = InnoDB;
 
 
@@ -63,10 +50,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`property_location` (
                                                           `city_id` INT NOT NULL,
                                                           `neighborhood_id` INT NOT NULL,
                                                           PRIMARY KEY (`property_location_id`),
-    INDEX `FK_property_id` (`property_id` ASC) INVISIBLE,
-    INDEX `FK_owner_id` (`owner_id` ASC) INVISIBLE,
-    INDEX `FK_city_id` (`city_id` ASC) INVISIBLE,
-    INDEX `FK_neighborhood_id` (`neighborhood_id` ASC) VISIBLE,
+    INDEX `FK_property_id` (`property_id` ASC) ,
+    INDEX `FK_owner_id` (`owner_id` ASC) ,
+    INDEX `FK_city_id` (`city_id` ASC) ,
+    INDEX `FK_neighborhood_id` (`neighborhood_id` ASC) ,
     CONSTRAINT `fk_property_location_property`
     FOREIGN KEY (`property_id`)
     REFERENCES `mydb`.`property` (`property_id`)
@@ -152,8 +139,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`transaction` (
     `commission` DECIMAL(10,2) NOT NULL,
     `transaction_type` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`transaction_id`),
-    INDEX `FK_property_id` (`property_id` ASC) INVISIBLE,
-    INDEX `FK_client_id` (`client_id` ASC) VISIBLE,
+    INDEX `FK_property_id` (`property_id` ASC) ,
+    INDEX `FK_client_id` (`client_id` ASC) ,
     CONSTRAINT `fk_transaction_property1`
     FOREIGN KEY (`property_id`)
     REFERENCES `mydb`.`property` (`property_id`)
@@ -176,15 +163,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`commission` (
     `percentage` DECIMAL(10,2) NOT NULL,
     `transaction_id` INT NOT NULL,
     PRIMARY KEY (`commission_id`),
-    INDEX `FK_transaction_id` (`transaction_id` ASC) VISIBLE,
+    INDEX `FK_transaction_id` (`transaction_id` ASC) ,
     CONSTRAINT `fk_commission_transaction1`
     FOREIGN KEY (`transaction_id`)
     REFERENCES `mydb`.`transaction` (`transaction_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
     ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
